5145f9b63c24ac779feb3bfb7ca6a711c0c76ac7
s7aesis <ryancd774@gmail.com>
2015-02-01 10:57:47 -0500

    card sprites added, basic project initialization

---
+++ Assets/Code/Main.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+
+    
+    // Use this for initialization
+	void Start () {
+        print("hello world");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
