c2e58338790f4534c32a3ca5f0087c96ff899bb3
s7aesis <ryancd774@gmail.com>
2015-02-02 14:52:13 -0500

    Drawing class linked, and sprites can be drawn from code

---
+++ Assets/Code/Drawing.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Drawing : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Method()
+    {
+        Debug.Log("Method Triggered");
+    }
+}
--- Assets/Code/Main.cs
+++ Assets/Code/Main.cs
@@ -2,11 +2,33 @@
 using System.Collections;
 
 public class Main : MonoBehaviour {
-    GameObject go;
+    public Sprite[] cardSprites;
+
     private Deck deck = null;
+    //private Drawing draw = new Drawing();
+    public Drawing draw;
+    
+
+    void Awake()
+    {
+        draw = gameObject.AddComponent<Drawing>();
+        draw.Method();
+        cardSprites = Resources.LoadAll<Sprite>("playingCards");
+    }
+    
     // Use this for initialization
 	void Start () {
         print("hello world");
+        draw.Method();
+        //GameObject.Find("PlayerCard0").GetComponent("SpriteRenderer").sprite = sprites.GetSprite("Sprite1");
+        GameObject go = new GameObject();
+        //go = GameObject.Find("PlayerCardT");// new GameObject("go");
+        //go.AddComponent("SpriteRenderer");
+        go.AddComponent<SpriteRenderer>();
+        //go.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("JD");
+        go.GetComponent<SpriteRenderer>().sprite = cardSprites[12];
+        
+        //go.GetComponent<SpriteRenderer>().sprite = sprites.GetSprite("Sprite1");
 
         deck = new Deck();
         
